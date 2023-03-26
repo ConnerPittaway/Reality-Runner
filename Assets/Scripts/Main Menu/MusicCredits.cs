c83e7163d4bdc9b77193183e05aa54eebed16495
Conner Pittaway <57526209+ConnerPittaway@users.noreply.github.com>
2023-03-26 18:33:51 +0100

    Music Credits Links

--- Assets/Scripts/Main Menu/MusicCredits.cs
+++ Assets/Scripts/Main Menu/MusicCredits.cs
@@ -4,16 +4,36 @@ using UnityEngine;
 
 public class MusicCredits : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    //Poixone
+    public void OpenPoixoneYouTube()
     {
-        
+        Application.OpenURL("https://www.youtube.com/@poixone");
     }
 
-    // Update is called once per frame
-    void Update()
+    public void OpenPoixoneSoundcloud()
     {
-        
+        Application.OpenURL("https://soundcloud.com/poixone");
+    }
+
+    public void OpenPoixoneSpotify()
+    {
+        Application.OpenURL("https://open.spotify.com/artist/1vXltCW0Wqb52h9dV8KoPV?autoplay=true");
+    }
+
+    //SVRGE
+    public void OpenSVRGEYouTube()
+    {
+        Application.OpenURL("https://www.youtube.com/channel/UCjRkBjbbkXF4jQf9DJkKk7Q");
+    }
+
+    public void OpenSVRGESoundcloud()
+    {
+        Application.OpenURL("https://soundcloud.com/svrge_official");
+    }
+
+    public void OpenSVRGESpotify()
+    {
+        Application.OpenURL("https://open.spotify.com/artist/4Qv9cmrt7qbn04iMQN7o5W");
     }
 
     void OnEnable()
