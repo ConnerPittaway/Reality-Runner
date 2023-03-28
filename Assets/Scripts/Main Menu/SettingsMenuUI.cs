0af8866951af003e193f26bec806e15eebb46140
Conner Pittaway <57526209+ConnerPittaway@users.noreply.github.com>
2023-03-28 17:39:11 +0100

    Imported Facebook SDK

--- Assets/Scripts/Main Menu/SettingsMenuUI.cs
+++ Assets/Scripts/Main Menu/SettingsMenuUI.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class SettingsMenu : MonoBehaviour
+public class SettingsMenuUI : MonoBehaviour
 {
     public Slider audioSilder;
     public GameObject muteButton, unmuteButton, frameButtonOn, frameButtonOff, languageScreen;
