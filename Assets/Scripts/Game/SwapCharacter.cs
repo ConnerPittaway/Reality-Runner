ee1f1226f27ea8fee4ec65d1c4fb4d9901f619d7
Conner Pittaway <57526209+ConnerPittaway@users.noreply.github.com>
2023-05-01 00:08:31 +0100

    Edited Swap Script, Added Force field animation

--- Assets/Scripts/Game/SwapCharacter.cs
+++ Assets/Scripts/Game/SwapCharacter.cs
@@ -4,55 +4,18 @@ using UnityEngine;
 
 public class SwapCharacter : MonoBehaviour
 {
-    public AnimatorOverrideController shroud;
-    public AnimatorOverrideController boxy;
-    public AnimatorOverrideController fiona;
+    public SerializableDictionary<GlobalDataManager.Characters, AnimatorOverrideController> characters;
 
     // Start is called before the first frame update
     void Start()
     {
-        if(GlobalDataManager.Instance == null)
+        if (GlobalDataManager.Instance == null)
         {
-            LoadShroud();
+            GetComponent<Animator>().runtimeAnimatorController = characters[0] as RuntimeAnimatorController;
         }
         else
         {
-            switch(GlobalDataManager.Instance.currentlySelectedCharacter)
-            {
-                case GlobalDataManager.Characters.SHROUD:
-                    LoadShroud();
-                    break;
-                case GlobalDataManager.Characters.BOXY:
-                    LoadBoxy();
-                    break;
-                case GlobalDataManager.Characters.FIONA:
-                    LoadFiona();
-                    break;
-                default:
-                    Debug.LogError("No Character Assigned");
-                    break;
-            }
+            GetComponent<Animator>().runtimeAnimatorController = characters[GlobalDataManager.Instance.currentlySelectedCharacter] as RuntimeAnimatorController;
         }
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
-    public void LoadShroud()
-    {
-        GetComponent<Animator>().runtimeAnimatorController = shroud as RuntimeAnimatorController;
-    }
-
-    public void LoadBoxy()
-    {
-        GetComponent<Animator>().runtimeAnimatorController = boxy as RuntimeAnimatorController;
-    }
-
-    public void LoadFiona()
-    {
-        GetComponent<Animator>().runtimeAnimatorController = fiona as RuntimeAnimatorController;
     }
 }
