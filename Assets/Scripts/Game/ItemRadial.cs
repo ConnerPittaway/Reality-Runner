68eae55e07c6dd2dc8f223679fd9744956fff2ca
Conner Pittaway <57526209+ConnerPittaway@users.noreply.github.com>
2023-05-02 20:43:13 +0100

    Forcefield Timer Game

--- Assets/Scripts/Game/ItemRadial.cs
+++ Assets/Scripts/Game/ItemRadial.cs
@@ -18,11 +18,32 @@ public class ItemRadial : MonoBehaviour
 
     //Item Effects
     public GameObject forcefieldEffect;
+    public float forcefieldTime;
 
     // Start is called before the first frame update
     void Start()
     {
         startedRoutines = false;
+
+        //Set Item Speed
+        switch (GlobalDataManager.Instance.currentShieldLevel)
+        {
+            case GlobalDataManager.ShieldLevel.LEVEL1:
+                forcefieldTime = 1.5f;
+                break;
+            case GlobalDataManager.ShieldLevel.LEVEL2:
+                forcefieldTime = 2f;
+                break;
+            case GlobalDataManager.ShieldLevel.LEVEL3:
+                forcefieldTime = 2.5f;
+                break;
+            case GlobalDataManager.ShieldLevel.LEVEL4:
+                forcefieldTime = 3f;
+                break;
+            case GlobalDataManager.ShieldLevel.LEVEL5:
+                forcefieldTime = 3.5f;
+                break;
+        }
     }
 
     public void useItem()
@@ -33,15 +54,17 @@ public class ItemRadial : MonoBehaviour
         currentAmount = 100.0f;
         itemButton.interactable = false;
         startedRoutines = false;
+        float speed = 0.0f;
 
         switch(player.activeItem)
         {
             case PlayerController.ItemTypes.SHIELD:
                 forcefieldEffect.SetActive(true);
+                speed = forcefieldTime;
                 break;
         }
 
-        LeanTween.value(gameObject, currentAmount, 0, 2.5f).setOnUpdate((float val)=>
+        LeanTween.value(gameObject, currentAmount, 0, speed).setOnUpdate((float val)=>
         {
             currentAmount = val;
         }).setOnComplete(itemFinished);
